652cffed1adf1078db018f546a94a112102779e8
ldy3112 <lundyrasuli03@gmail.com>
2022-07-08 14:39:52 +0700

    Done

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,4 +1,5 @@
 var isProfilEmbed = 1;
+var isMobile = 0;
 
 var contacts = [
     {
@@ -15,10 +16,31 @@ var contacts = [
     },
     {
         name: "Lala"
+    },
+    {
+        name: "Stallman"
+    },
+    {
+        name: "Yo"
     }
 ]
 
 var messageJustin = [
+    {
+        by: 1,
+        type: 'txt',
+        value: 'P'
+    },
+    {
+        by: 1,
+        type: 'txt',
+        value: 'P'
+    },
+    {
+        by: 1,
+        type: 'txt',
+        value: 'P'
+    },
     {
         by: 1,
         type: 'txt',
@@ -127,9 +149,88 @@ var messageLala = [
     }
 ]
 
+var messageStallman = [
+    {
+        by: 1,
+        type: 'txt',
+        value: "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
+    },
+    {
+        by: 1,
+        type: 'txt',
+        value: "Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project."
+    },
+    {
+        by: 1,
+        type: 'txt',
+        value: "There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called \"Linux\" distributions are really distributions of GNU/Linux."
+    },
+    {
+        by: 0,
+        type: 'txt',
+        value: "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
+    },
+    {
+        by: 0,
+        type: 'txt',
+        value: "Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project."
+    },
+    {
+        by: 0,
+        type: 'txt',
+        value: "There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called \"Linux\" distributions are really distributions of GNU/Linux."
+    },
+]
 
+var messageYo = [
+    {
+        by: 1,
+        type: 'txt',
+        value: "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
+    },
+    {
+        by: 1,
+        type: 'txt',
+        value: "Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project."
+    },
+    {
+        by: 1,
+        type: 'txt',
+        value: "There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called \"Linux\" distributions are really distributions of GNU/Linux."
+    },
+    {
+        by: 0,
+        type: 'txt',
+        value: "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
+    },
+    {
+        by: 0,
+        type: 'txt',
+        value: "Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project."
+    },
+    {
+        by: 0,
+        type: 'txt',
+        value: "There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called \"Linux\" distributions are really distributions of GNU/Linux."
+    },
+]
+
+function moBack() {
+    if (isMobile === 1) {
+        document.getElementById("chat").style.opacity = "0";
+        setTimeout(function () {
+            document.getElementById("sidepane").style.width = "100%";
+            document.getElementById("main-area").style.width = "0%";
+        }, 100);
+    }
+}
 
 function showChat(name) {
+    if (isMobile === 1) {
+        document.getElementById("sidepane").style.width = "0%";
+        document.getElementById("main-area").style.width = "100%";
+        setTimeout(function () { document.getElementById("chat").style.opacity = "1"; }, 300);
+    }
     const chatArea = document.getElementById("chat");
     chatArea.innerHTML = '';
     var isProfilEmbed = 1;
@@ -147,8 +248,11 @@ function printChat(who, mode, message, name) {
     const messageText = document.createTextNode(message);
 
     if (who === 0) {
-        parent.setAttribute('class', 'chat ralign')
+        parent.setAttribute('class', 'chat alignyou')
         span.setAttribute('class', 'bg-dark you')
+        if (isProfilEmbed === 0) {
+            parent.setAttribute('class', 'chat alignyou chat-switch')
+        }
         isProfilEmbed = 1;
         decideMode()
     }
@@ -165,11 +269,11 @@ function printChat(who, mode, message, name) {
 
             parent.appendChild(profilepict);
             isProfilEmbed = 0;
-            parent.setAttribute('class', 'chat lalign withprof');
+            parent.setAttribute('class', 'chat chat-switch');
             decideMode()
         }
         else {
-            parent.setAttribute('class', 'chat lalign chatonly');
+            parent.setAttribute('class', 'chat chatonly');
             decideMode()
         }
     }
@@ -203,20 +307,28 @@ function listAllChat() {
 
 function summonChat(name) {
     eval("var lastChatN = message" + name + ".length - 1")
-    eval("message" + name + ".map(({value, type, by},i)=> if(i === lastChatN){};")
-
+    eval("message" + name + ".map(function({by,type,value},i){ if(i === lastChatN){previewBuilder(by,type,value);}})")
+    function previewBuilder(by, type, value) {
+        var who = ["You : ", ""]
+        if (type === 'sticker') {
+            createList(who[by] + "Sticker");
+        }
+        else {
+            createList(who[by] + value);
+        }
+    }
     function createList(lastChat) {
-        console.log(name + " " + chatLen + " " + lastChat)
 
         // Create parent
         const lists = document.createElement("div");
-        lists.setAttribute('class', 'profile chats');
+        lists.setAttribute('class', 'clickable profile chatlists');
         lists.setAttribute('onclick', 'showChat("' + name + '")')
 
         // Create PP
         const chatpp = document.createElement("img");
         chatpp.src = "assets/images/icons/" + name + ".jpg";
         chatpp.setAttribute('alt', name + '-photo');
+        chatpp.setAttribute('onerror', 'this.onerror=null; this.src=\'assets/images/icons/default.png\'')
 
         // Create chat preview
         const chatPreviewParent = document.createElement("div");
@@ -231,7 +343,7 @@ function summonChat(name) {
         const chatPreview = document.createElement("span");
         chatPreview.setAttribute('class', 'preview-text');
 
-        const chatPreviewText = document.createTextNode(name);
+        const chatPreviewText = document.createTextNode(lastChat);
         chatPreview.appendChild(chatPreviewText);
 
         const timeStamp = document.createElement("span");
@@ -251,5 +363,27 @@ function summonChat(name) {
         document.getElementById("list").appendChild(lists);
     }
 }
+
+function modifyMobile(x) {
+    if (x.matches) { // If media query matches
+        document.getElementById("sidepane").style.width = "100%"
+        document.getElementById("main-area").style.width = "0%"
+        document.getElementById("chat").style.opacity = "0"
+        document.getElementById("moBack").style.width = "1em";
+        document.getElementById("moBack").style.display = "inline";
+        isMobile = 1;
+    } else {
+        document.getElementById("sidepane").style.width = "40%"
+        document.getElementById("main-area").style.width = "60%"
+        document.getElementById("chat").style.opacity = "1"
+        document.getElementById("moBack").style.width = "0em";
+        setTimeout(function () { document.getElementById("moBack").style.display = "none"; })
+        isMobile = 0;
+    }
+}
+
+var x = window.matchMedia("(max-width: 530px)");
+modifyMobile(x) // Call listener function at run time
+x.addListener(modifyMobile) // Attach listener function on state changes
 
 listAllChat();
