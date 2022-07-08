832088d64bda1c1d6469886e1a5115fa2681c021
ldy3112 <lundyrasuli03@gmail.com>
2022-07-08 18:25:25 +0700

    Divide each component

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -215,6 +215,41 @@ var messageYo = [
     },
 ]
 
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+
+            var x = window.matchMedia("(max-width: 530px)");
+            modifyMobile(x) // Call listener function at run time
+            x.addListener(modifyMobile) // Attach listener function on state changes
+            listAllChat();
+
+            return;
+        }
+    }
+}
+
+
 function moBack() {
     if (isMobile === 1) {
         document.getElementById("chat").style.opacity = "0";
@@ -382,8 +417,8 @@ function modifyMobile(x) {
     }
 }
 
-var x = window.matchMedia("(max-width: 530px)");
-modifyMobile(x) // Call listener function at run time
-x.addListener(modifyMobile) // Attach listener function on state changes
+includeHTML()
 
-listAllChat();
+setTimeout(function () {
+
+}, 200)
